826dbb023fb3f1968dec024d2312b5bc5ea5e84d
Vasily Naumkin <bezumkin@yandex.ru>
2019-03-20 14:40:08 +0700

    Add `mailto` only for links without any protocol

--- src/Jevix.php
+++ src/Jevix.php
@@ -1536,7 +1536,7 @@ class Jevix
                                 $value = 'http://' . $value;
                             } elseif (preg_match('/.+@.+\..+/i', $value)) {
                                 // Но нет протокола - добавляем
-                                if (!preg_match('/^(mailto):/ui', $value)) {
+                                if (!preg_match('/^(.*?):/ui', $value)) {
                                     $value = 'mailto:' . $value;
                                 }
                             } // Если нет указания протокола:
